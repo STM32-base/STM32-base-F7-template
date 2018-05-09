7f852e964a041c1d2842aaa7dabc3131d0fcf6ad
Thomas Gravekamp <me@thomas-gravekamp.nl>
2018-05-10 00:28:57 +0200

    Add initial files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# STM32-base F7 template
+
+This repository is a project template for using a STM32F7 device with the STM32-base project. See the [STM32-base repository](https://github.com/ThomasGravekamp/STM32-base) for more information on how to get started with this template repository.
---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include "STM32F7xx.h"
+#include "STM32F722xx.h"
+
+// Quick and dirty delay
+static void delay (unsigned int time) {
+    for (unsigned int i = 0; i < time; i++)
+        for (volatile unsigned int j = 0; j < 2000; j++);
+}
+
+int main (void) {
+    // Turn on the GPIOC peripheral
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+
+    // Put the pin in genreal purpose output mode
+    GPIOC->MODER |= GPIO_MODER_MODER13_0;
+
+    while (1) {
+        // Reset the bit for port C13
+        GPIOC->BSRR = GPIO_BSRR_BR_13;
+
+        delay(500);
+
+        // Set the bit for port C13
+        GPIOC->BSRR = GPIO_BSRR_BS_13;
+
+        delay(500);
+    }
+}
